132b6ceee02b612f7aa2c3ca5a57df72c2bd4da4
Meshach <fridaymeshach@gmail.com>
2025-09-30 00:39:18 +0100

    added auth page

---
+++ src/pages/AuthPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <BodySection />
+</template>
+
+<script>
+import BodySection from '@/components/auth/BodySection.vue';
+
+export default {
+    name: 'AuthPage',
+    components: {
+        BodySection,
+    },
+    data() {
+    return {
+      savedChanges: false,
+    };
+  },
+  provide() {
+    return {
+      savedChanges: this.savedChanges,
+    };
+  },
+  mounted() {
+    document.body.classList.add('auth-page');
+},
+beforeUnmount() {
+    document.body.classList.remove('auth-page');
+},
+}
+</script>
+
+<style scoped>
+@import '@/assets/css/auth.css';
+</style>
---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <!-- design a good looking 404 page -->
+    <div class="not-found">
+        <h1>404 Not Found</h1>
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <router-link to="/" class="btn btn-primary">Go to Home</router-link>
+    </div>
+</template>
+    
+<script>
+export default {
+    name: 'NotFoundPage'
+}
+</script>
+
+<style scoped>
+    .not-found {
+        text-align: center;
+        margin-top: 100px;
+    }
+    .btn {
+        margin-top: 20px;
+    }
+</style>
